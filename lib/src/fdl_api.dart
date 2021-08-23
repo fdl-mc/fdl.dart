a36fd184b33177dfa9a0f01d768927b9a268c2cb
Arsenii Liunsha <me@arslee.dev>
2021-08-23 20:06:37 +0900

    feat: Implement FDL API interface

--- lib/src/fdl_api.dart
+++ lib/src/fdl_api.dart
@@ -4,3 +4,18 @@ abstract class IFdlApi {
   Future<ServerStats> getMainServerStats();
   Future<PaymentResponse> pay(PaymentBuilder paymentBuilder);
 }
+
+class FdlApi implements IFdlApi {
+  final _RestEndpoints _endpoints;
+  FdlApi(String baseUrl) : _endpoints = _RestEndpoints(baseUrl);
+
+  @override
+  Future<ServerStats> getMainServerStats() async {
+    return await _endpoints.getMainServerStats();
+  }
+
+  @override
+  Future<PaymentResponse> pay(PaymentBuilder paymentBuilder) async {
+    return await _endpoints.pay(paymentBuilder);
+  }
+}
